e81c98f055c75ef03447b89e825908bb86b2a44a
shirza <zagurishir@gmail.com>
2021-08-09 13:27:07 +0300

    Release at 08/09/2021 13:27:06

--- deploy.js
+++ deploy.js
@@ -1,6 +1,9 @@
-const moment = require('moment');
-const execSync = require('child_process').execSync;
+const moment = require("moment");
+const execSync = require("child_process").execSync;
 
-const dateTime = moment().format('MM/DD/YYYY HH:mm:ss');
+const dateTime = moment().format("MM/DD/YYYY HH:mm:ss");
 
-execSync(`webpack && git add . && git commit -m \"Release at ${dateTime}\" && git push && npm publish`, { stdio: [0, 1, 2]} );
+execSync(
+  `webpack && git add . && git commit -m \"Release at ${dateTime}\" && git push && npm publish`,
+  { stdio: [0, 1, 2] }
+);
